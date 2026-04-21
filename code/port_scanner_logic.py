806417f114982d902d496e34a9c5ea03b201260b
Andrea <andrea.15gros@gmail.com>
2026-04-21 21:54:37 +0200

    Modified device_scan

--- code/port_scanner_logic.py
+++ code/port_scanner_logic.py
@@ -10,10 +10,31 @@ class Scanner:
         for snd, rcv in ans:
             print(f"Protocol {snd.proto} answered from {rcv.src}")
     
-    def device_scan(self, target = "127.0.0.1"):
-        ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target), timeout=2)
-        ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%") )
+    def device_scan(self, target="192.168.1.1/24"):
+        ans, unans = srp(
+            Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target),
+            timeout = 1,
+            verbose=False
+        )
+        
+        devices = []
+        for sent, received in ans:
+            device = {
+                "ip": received[ARP].psrc,
+                "mac": received[Ether].src
+            }
+            devices.append(device)
+            print(f"IP: {device['ip']:<20} MAC: {device['mac']}")
+        
+        print(f"\Found {len(devices)} devices")
+        return devices
+
+    def ping(self, target = "127.0.0.1", nPkt=1):
+        ans, unans = sr(IP(dst=target)/ICMP(), timeout=3)
+        ans.summary(lambda s,r: r.sprintf("%IP.src% is alive") )
+
+    
 
 scan = Scanner()
 
-scan.device_scan("10.216.10.117")
+scan.device_scan("192.168.1.1/24")
