eb603aaa412fa4a13f70408ca5278323be5133a8
Andrea <andrea.15gros@gmail.com>
2026-05-18 11:41:30 +0200

    added deploy.sh

---
+++ code/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Vai nella cartella del progetto (opzionale ma consigliato)
+cd
+
+cd Sniffable
+
+# Avvia il server con il python del venv
+./venv/bin/python ./code/server.py
+
+npm start
