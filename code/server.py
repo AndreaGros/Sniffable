cccb037ceb12437da8c744aad9d1a3b5dbfd5f08
Andrea <andrea.15gros@gmail.com>
2026-05-25 11:47:28 +0200

    code now is more clear

--- code/server.py
+++ code/server.py
@@ -17,11 +17,13 @@ def server_callback(data, loop):
     except asyncio.QueueFull:
         pass  # drop packet se troppo traffico
 
+
 async def packet_stream(websocket):
     while True:
         packet = await packet_queue.get()
 
         await websocket.send(json.dumps({"type": "packet", "data": packet}))
+
 
 def packet_from_json(data: dict, iface=None):
 
@@ -29,18 +31,13 @@ def packet_from_json(data: dict, iface=None):
     eth = data.get("eth")
 
     if eth:
-        sender.add_ether(
-            dst=eth.get("dst", "ff:ff:ff:ff:ff:ff"),
-            src=eth.get("src")
-        )
+        sender.add_ether(dst=eth.get("dst", "ff:ff:ff:ff:ff:ff"), src=eth.get("src"))
 
     ip = data.get("ip")
 
     if ip:
         sender.add_ip(
-            dst=ip.get("dst", "127.0.0.1"),
-            src=ip.get("src"),
-            ttl=ip.get("ttl", 64)
+            dst=ip.get("dst", "127.0.0.1"), src=ip.get("src"), ttl=ip.get("ttl", 64)
         )
 
     transport = data.get("transport")
@@ -54,13 +51,12 @@ def packet_from_json(data: dict, iface=None):
                 dport=transport.get("dstPort", 80),
                 sport=transport.get("srcPort"),
                 flags=transport.get("flags", "S"),
-                seq=transport.get("seq")
+                seq=transport.get("seq"),
             )
 
         elif proto == "UDP":
             sender.add_udp(
-                dport=transport.get("dstPort", 80),
-                sport=transport.get("srcPort")
+                dport=transport.get("dstPort", 80), sport=transport.get("srcPort")
             )
 
         elif proto == "ICMP":
@@ -74,67 +70,89 @@ def packet_from_json(data: dict, iface=None):
     return sender
 
 
+async def handle_start_sniffer(websocket, data):
+    sniffer.start()
+    await websocket.send(json.dumps({"type": "status", "data": "sniffer_started"}))
+
+
+async def handle_stop_sniffer(websocket, data):
+    sniffer.stop()
+    await websocket.send(json.dumps({"type": "status", "data": "sniffer_stopped"}))
+
+
+async def handle_clear_packets(websocket, data):
+    sniffer.packets = {}
+    sniffer.index = 0
+
+    await websocket.send(json.dumps({"type": "status", "data": "cleared"}))
+
+
+async def handle_packet_detail(websocket, data):
+    pkt = sniffer.selectSinglePacket(data.get("packet_id"))
+
+    await websocket.send(json.dumps({"type": "detail", "data": pkt}))
+
+
+async def handle_get_hosts(websocket, data):
+    await websocket.send(json.dumps({"type": "status", "data": "scan_started"}))
+
+    loop = asyncio.get_running_loop()
+
+    devices = await loop.run_in_executor(None, scanner.device_scan, data.get("target"))
+
+    await websocket.send(json.dumps({"type": "devices", "data": devices}))
+
+
+async def handle_port_scan(websocket, data):
+    target = data.get("target")
+    start_port = int(data.get("startPort"))
+    end_port = int(data.get("endPort"))
+
+    loop = asyncio.get_running_loop()
+
+    ports = await loop.run_in_executor(
+        None, scanner.port_scan, target, start_port, end_port
+    )
+
+    await websocket.send(json.dumps({"type": "ports", "data": ports}))
+
+
+async def handle_sender(websocket, data):
+    pkt_structure = data.get("pkt")
+
+    sender_obj = packet_from_json(pkt_structure)
+
+    pkt = sender_obj.build_packet()
+
+    if pkt:
+        sender_obj.send(pkt)
+
+
+handlers = {
+    "start_sniffer": handle_start_sniffer,
+    "stop_sniffer": handle_stop_sniffer,
+    "clear_packets": handle_clear_packets,
+    "packet_detail": handle_packet_detail,
+    "get_hosts": handle_get_hosts,
+    "port_scan": handle_port_scan,
+    "sender": handle_sender,
+}
+
+
 async def handler(websocket):
     stream_task = asyncio.create_task(packet_stream(websocket))
 
     try:
         async for message in websocket:
             data = json.loads(message)
-
             action = data.get("action")
-
-            if action == "start_sniffer":
-                sniffer.start()
+            handler_func = handlers.get(action)
+            if not handler_func:
                 await websocket.send(
-                    json.dumps({"type": "status", "data": "sniffer_started"})
+                    json.dumps({"type": "error", "data": f"Unknown action: {action}"})
                 )
-
-            elif action == "stop_sniffer":
-                sniffer.stop()
-                await websocket.send(json.dumps({"type": "status", "data": "sniffer_stopped"}))
-
-            elif action == "clear_packets":
-                sniffer.packets = {}
-                sniffer.index = 0
-                await websocket.send(json.dumps({"type": "status", "data": "cleared"}))
-
-            elif action == "packet_detail":
-                pkt = sniffer.selectSinglePacket(data.get("packet_id"))
-                print(pkt)
-                await websocket.send(json.dumps({"type": "detail", "data": pkt}))
-
-            elif action == "get_hosts":
-                await websocket.send(json.dumps({"type": "status", "data": "scan_started"}))
-                loop = asyncio.get_running_loop()
-                devices = await loop.run_in_executor(
-                    None,
-                    scanner.device_scan,
-                    data.get("target")
-                )
-                print("target: ", data.get("target"))
-                await websocket.send(json.dumps({"type": "devices", "data": devices}))
-
-            elif action == "port_scan":
-                target = data.get("target")
-                startPort = int(data.get("startPort"))
-                endPort = int(data.get("endPort"))
-                loop = asyncio.get_running_loop()
-                ports = await loop.run_in_executor(
-                    None,
-                    scanner.port_scan,
-                    target,
-                    startPort,
-                    endPort
-                )
-                await websocket.send(json.dumps({"type": "ports", "data": ports}))
-            
-            elif action == "sender":
-                pktStructure = data.get("pkt")
-                sender_obj = packet_from_json(pktStructure)
-                pkt = sender_obj.build_packet()
-                if pkt:
-                    sender_obj.send(pkt)
-
+                continue
+            await handler_func(websocket, data)
 
     finally:
         stream_task.cancel()
